df079ad3b9215117dcece8442354e3a42e4264c2
Hiro Asari <asari.ruby@gmail.com>
2010-12-05 14:19:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baremetal::Application.config.secret_token = '26d9fe0bc9c91c4b327cac9e801d1613db26654c41a2aad0ba400a9db637f718ad8e09a0c16272242248867f6803911931c4a5bdac58b191650eb9708db71d2e'
